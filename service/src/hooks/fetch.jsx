8f1292e0bdc2406555f9fa9b3c2db45d76040ad6
Kevin Amado <kamadorueda@gmail.com>
2020-11-01 17:58:37 -0500

    solve warnings

--- service/src/hooks/fetch.jsx
+++ service/src/hooks/fetch.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 
+const fetchJSON = async (url) => {
+  const response = await fetch(url);
+
+  return await response.json();
+};
+
 export const useFetchJSON = (url, defaultData) => {
   const [data, setData] = useState(defaultData);
 
-  const getAnswer = async () => {
-    const res = await fetch(url);
-    setData(await res.json());
-  };
-
   useEffect(() => {
-    getAnswer();
-  }, []);
+    setData(fetchJSON(url));
+  }, [url]);
 
   return data;
 }
